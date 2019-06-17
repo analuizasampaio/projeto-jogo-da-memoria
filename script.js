68498c02244cf76a8907958d1c0174c53a1244c7
analuizasampaio <sampaioaanaluiza@gmail.com>
2019-06-17 15:10:30 -0300

    Add lista com comidas e for

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 const cartas = [
     {
-        "nome": "",
-        "imagem": "",
+        "nome": "batata",
+        "imagem":"images/batata.jpg" ,
         "id": 1,
-        },
+    },
     {
-        "nome": "",
-        "imagem": "",
+        "nome": "coxinha",
+        "imagem":"images/coxinha.jpg" ,
         "id": 2,
     },
     {
-        "nome": "",
-        "imagem": "",
+        "nome": "donuts",
+        "imagem": "donuts.png",
         "id": 3,
     },
     {
-        "nome": "",
-        "imagem": "",
+        "nome": "lasanha",
+        "imagem": "images/lasanha.jpg",
         "id": 4,
     },
     {
-        "nome": "",
-        "imagem": "",
+        "nome": "pipoca",
+        "imagem": "images/pipoca.jpg",
         "id": 5,
     }    
 ]
+
+let container = document.querySelector(".container")
+
+for (let i = 0; i<=cartas.length; i++){
+    let box = document.createElement("div")
+    box.setAttribute = ("class", cartas[i].nome)
+    box.innerHTML = `
+        <img src="${cartas[i].imagem}" alt="">
+        <img src="" alt="">
+    `
+    container.appendChild(box)
+
+}
+
