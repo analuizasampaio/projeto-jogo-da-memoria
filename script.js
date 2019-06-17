8fe965d2f42bdc69b3fe12db1a3fca7feb70e5ff
analuizasampaio <sampaioaanaluiza@gmail.com>
2019-06-17 15:32:14 -0300

    Adicionar lista2

--- script.js
+++ script.js
@@ -26,6 +26,36 @@ const cartas = [
     }    
 ]
 
+
+const cartas2 = [
+    {
+        "nome": "batata",
+        "imagem":"images/batata.jpg" ,
+        "id": 1,
+    },
+    {
+        "nome": "coxinha",
+        "imagem":"images/coxinha.jpg" ,
+        "id": 2,
+    },
+    {
+        "nome": "donuts",
+        "imagem": "donuts.png",
+        "id": 3,
+    },
+    {
+        "nome": "lasanha",
+        "imagem": "images/lasanha.jpg",
+        "id": 4,
+    },
+    {
+        "nome": "pipoca",
+        "imagem": "images/pipoca.jpg",
+        "id": 5,
+    }    
+    
+]
+
 let container = document.querySelector(".container")
 
 for (let i = 0; i<=cartas.length; i++){
@@ -33,9 +63,11 @@ for (let i = 0; i<=cartas.length; i++){
     box.setAttribute = ("class", cartas[i].nome)
     box.innerHTML = `
         <img src="${cartas[i].imagem}" alt="">
-        <img src="" alt="">
+        <img src="images/Mastertech.png" alt="">
     `
     container.appendChild(box)
 
 }
+
+
 
